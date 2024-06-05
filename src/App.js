dc1e868bfdfaf0418ab1d4c68ea635b501aac651
Gleb Akunenko <akunenko.study@gmail.com>
2024-06-05 19:42:43 +0300

    Edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'; // Импорт CSS файла
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [formData, setFormData] = useState({ username: '', email: '' });
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = () => {
+    fetch('http://localhost:4444/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error fetching users:', error));
+  };
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:4444/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      if (response.ok) {
+        fetchUsers();
+        setShowForm(false);
+        setFormData({ username: '', email: '' });
+      } else {
+        alert('Error adding user:' + response.statusText);
+      }
+    } catch (error) {
+      alert('Error adding user:' + error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to my Pet Project</h1>
       </header>
+      <main>
+        <h2>Users</h2>
+        <div className="table-container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map(user => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <button onClick={() => setShowForm(true)}>Добавить нового пользователя</button>
+        {showForm && (
+          <form onSubmit={handleSubmit}>
+            <label>
+              Username:
+              <input type="text" name="username" value={formData.username} onChange={handleInputChange} />
+            </label>
+            <label>
+              Email:
+              <input type="email" name="email" value={formData.email} onChange={handleInputChange} />
+            </label>
+            <button type="submit">Принять</button>
+          </form>
+        )}
+      </main>
     </div>
   );
 }
